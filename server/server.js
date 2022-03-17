d743250e368b658c8330e2f2edc48e6151d43b5d
Laura Horsley <horsleyshorses02@gmail.com>
2022-03-17 13:16:50 -0700

    Connected Rollbar

--- server/server.js
+++ server/server.js
@@ -6,6 +6,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'c6ee333a6f464616a7d7fc92af1cd459',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 
 // Middleware
 app.use(express.static(path.join(__dirname, "../public")));
